06fe02fbc02c57763649220bcc0d5b205fe259f0
qinran0423 <1156107187@qq.com>
2021-12-05 16:52:38 +0800

    声明

--- Chapter2: JavaScript的语法/index.js
+++ Chapter2: JavaScript的语法/index.js
@@ -6,22 +6,67 @@
 // console.log(typeof 'test!'); // string、
 
 // ----
-var str = 'abcde'
-var obj = new String(str)
+// var str = 'abcde'
+// var obj = new String(str)
 
 
-function newToString() {
-  return 'hello world'
+// function newToString() {
+//   return 'hello world'
+// }
+
+// function func(val) {
+//   val.toString = newToString
+// }
+
+// func(str)
+// console.log(str); //abcde
+
+
+// func(obj)
+// console.log(String(obj)); // hello world
+
+// ----
+// 隐式声明
+// function foo() {
+//   value = 100;
+// }
+
+// foo()
+
+// var value = 100
+// function foo() {
+//   value = 1000
+// }
+
+// foo()
+
+// 解构
+// var [x, y] = [1, 2]
+
+// 模板字符串
+// foo = tpl => console.log(typeof tpl, tpl instanceof Array, tpl);
+// foo`xyz`
+// object true ['xyz']
+
+// TODO
+// console.log(String.raw({ raw: ['', ',\nworld'] }, 'Hi'));
+// // 等价于
+// foo = tpl => String.raw(tpl, 'hi')
+// console.log(foo`${1},
+// world`);
+
+
+
+// ----
+// function foo(x, y = 100) {
+//   console.log([x, y]);
+// }
+
+// foo('abc') // ['abc', 100]
+
+
+function foo(x = 'abc', y) {
+  console.log([x, y]);
 }
 
-function func(val) {
-  val.toString = newToString
-}
-
-func(str)
-console.log(str); //abcde
-
-
-func(obj)
-console.log(String(obj)); // hello world
-
+foo(undefined, 'abc') // ['abc','abc']
